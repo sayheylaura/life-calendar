5ee7fb7aa9fece2eb14f68e45e75cba7e0153ebc
Laura Ferrando <lauraferrandof@gmail.com>
2019-03-24 13:05:57 +0100

    Configure router. Change page language

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+import { CalendarComponent } from "./calendar/calendar.component";
+import { EditorComponent } from "./editor/editor.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "/calendar", pathMatch: "full" },
+  { path: "calendar", component: CalendarComponent },
+  { path: "editor", component: EditorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
